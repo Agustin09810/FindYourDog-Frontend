b86dd35b360ddbe4782624deaefa827771407198
Agustin09810 <a.lorenzogon2@gmail.com>
2022-10-16 00:35:08 -0300

    routing

--- final_project/src/app/app-routing.module.ts
+++ final_project/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { DogbyzoneComponent } from './components/dogbyzone/dogbyzone.component';
+import { ChatPreviewComponent } from './components/chat/chat-preview/chat-preview.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'zone/:zoneId', component: DogbyzoneComponent },
+  { path: 'chat', component: ChatPreviewComponent }
 
 ];
 
