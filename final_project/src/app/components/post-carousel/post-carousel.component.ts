95b157ea0b0687721b6cd676210109392781959c
tomasrama12 <88690021+tomasrama12@users.noreply.github.com>
2022-10-18 04:11:03 -0300

    carousel fixeado

--- final_project/src/app/components/post-carousel/post-carousel.component.ts
+++ final_project/src/app/components/post-carousel/post-carousel.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Image } from 'src/app/interfaces/Image';
 
 
 @Component({
   selector: 'app-post-carousel',
   templateUrl: './post-carousel.component.html',
+  //encapsulation: ViewEncapsulation.None,
   styleUrls: ['./post-carousel.component.css']
 })
 export class PostCarouselComponent implements OnInit {
 
   @Input() postImages?:Image[];
 
-
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
   constructor() { }
 
   ngOnInit(): void {
