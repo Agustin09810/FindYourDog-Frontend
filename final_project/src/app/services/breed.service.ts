ee6e95b1735375ac3eea49a5dc4bdf1570ac81ba
Agustin09810 <a.lorenzogon2@gmail.com>
2022-10-26 17:28:47 -0300

    breed step, almost finished

---
+++ final_project/src/app/services/breed.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Breed } from '../interfaces/Breed';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BreedService {
+  private breedsUrl = 'api/breeds';
+  constructor(private http: HttpClient) { }
+
+  getBreeds(): Observable<Breed[]> {
+    return this.http.get<Breed[]>(this.breedsUrl);
+  }
+
+  getBreedById(id: string): Observable<Breed|undefined> {
+    return this.getBreeds().pipe(map(breeds => breeds.find(breed => breed.id === id)));
+  }
+}
