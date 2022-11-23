2d517c9055f549e0b26689c67b146ca013aed078
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2022-11-23 19:27:22 -0300

    logout

--- final_project/src/app/components/navbar-sup/navbar-sup.component.ts
+++ final_project/src/app/components/navbar-sup/navbar-sup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar-sup',
@@ -12,7 +13,7 @@ export class NavbarSupComponent implements OnInit {
   @Input() backArrow: boolean = false;
 
   constructor(
-    private location: Location
+    private location: Location, private authService:AuthService
   ) { }
 
   ngOnInit(): void {
@@ -20,6 +21,10 @@ export class NavbarSupComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  logout(){
+    this.authService.logout();
   }
 
 }
