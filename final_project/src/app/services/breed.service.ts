42304f0833ebfe4729e35351259abc2cbbcff3c2
tomasrama12 <88690021+tomasrama12@users.noreply.github.com>
2022-11-27 17:02:25 -0300

    breeds handlers

--- final_project/src/app/services/breed.service.ts
+++ final_project/src/app/services/breed.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Breed } from '../interfaces/Breed';
-import { Observable, map, of } from 'rxjs';
+import { Observable, map, of, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +14,16 @@ export class BreedService {
 
   constructor(private http: HttpClient) { }
 
-  getBreeds(): Observable<Breed[]> {
-    return this.http.get<Breed[]>(this.breedsUrl);
+  getBreeds(): Observable<Breed[]|any> {
+    return this.http.get<Breed[]>(this.breedsUrl).pipe(catchError( err => {
+      return of(err);
+    }));
   }
 
-  getBreedById(id: string): Observable<Breed|undefined> {
-    return this.http.get<Breed|undefined>(`${this.breedsUrl}/${id}`);
+  getBreedById(id: string): Observable<Breed|undefined|any> {
+    return this.http.get<Breed|undefined>(`${this.breedsUrl}/${id}`).pipe(catchError( err => {
+      return of(err);
+      }));
   }
 
   getBreedByName(name: string): Observable<Breed[]> {
